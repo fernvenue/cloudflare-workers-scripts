3806d62bd83aed0767e8debe7de8f3f3eea40a17
fernvenue <git@fernvenue.com>
2022-06-10 18:56:47 +0800

    Update robots.js

--- robots.js
+++ robots.js
@@ -1,25 +1,14 @@
-const block = `user-agent: *
-disallow: /`;
-
-const allow = `user-agent: *
-allow: /`;
-
-const allowlist = ["allow1.example.com", "allow2.example.com"];
+const blockRule = `user-agent: *\ndisallow: /`;
+const allowRule = `user-agent: *\nallow: /`;
+const allowSites = ["allow1.example.com", "allow2.example.com"];
 
 async function handleRequest(request) {
   const requestURL = new URL(request.url);
-  const hostname = requestURL.hostname;
-  if (allowlist.includes(hostname) === true) {
-    return new Response(allow, {
-      headers: {
-        'content-type': 'text/plain;charset=UTF-8',
-      },
-    });
-  } return new Response(block, {
-    headers: {
-      'content-type': 'text/plain;charset=UTF-8',
-    },
-  });
+  const requestHostname = requestURL.hostname;
+  if (allowSites.includes(requestHostname) === true) {
+    return new Response(allowRule, { headers: { "content-type": "text/plain;charset=utf-8" }});
+  };
+  return new Response(blockRule, { headers: { "content-type": "text/plain;charset=utf-8" }});
 }
 
 addEventListener('fetch', event => {
