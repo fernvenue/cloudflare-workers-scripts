cb8b4da054fab46055e90a6e948a960fce7f7bd1
InsHomePgup <7328234@gmail.com>
2022-06-11 11:23:54 +0800

    pr

--- src/return-cloudflare-ray.js
+++ src/return-cloudflare-ray.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-	return new Response(request.headers.get('cf-connecting-ip'), {
+	return new Response(request.headers.get("cf-ray");, {
         headers: {
             "content-type": "text/plain;charset=utf-8"
         }
